14e05e730a4abd6fb55263892f58f07d20afed89
GaboRReyes <reyesjosafat816@gmail.com>
2026-03-13 09:19:59 -0600

    Creacion de las routas a los proveedores

--- oauth-login/routes/web.php
+++ oauth-login/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/auth/discord', [AuthController::class, 'redirectDiscord']);
+Route::get('/auth/discord/callback', [AuthController::class, 'handleDiscordCallback']);
+
+Route::get('/auth/spotify', [AuthController::class, 'redirectSpotify']);
+Route::get('/auth/spotify/callback', [AuthController::class, 'handleSpotifyCallback']);
