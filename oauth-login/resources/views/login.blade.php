f7376efbe21d09c58bb8a7743ea13d4f82f7c05e
GaboRReyes <reyesjosafat816@gmail.com>
2026-03-13 09:50:40 -0600

    Correccion de las rutas

---
+++ oauth-login/resources/views/login.blade.php
@@ -0,0 +1,40 @@
+<h1>Login OAuth</h1>
+
+<a href="/auth/discord" class="btn discord">
+    <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/discord.svg" width="20">
+    Iniciar sesión con Discord
+</a>
+
+<br><br>
+
+<a href="/auth/spotify" class="btn spotify">
+    <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/spotify.svg" width="20">
+    Iniciar sesión con Spotify
+</a>
+
+<style>
+
+.btn{
+    display:inline-flex;
+    align-items:center;
+    gap:10px;
+    padding:10px 16px;
+    text-decoration:none;
+    color:white;
+    border-radius:6px;
+    font-weight:bold;
+}
+
+.discord{
+    background:#5865F2;
+}
+
+.spotify{
+    background:#1DB954;
+}
+
+.btn img{
+    filter: invert(1);
+}
+
+</style>
--- oauth-login/routes/web.php
+++ oauth-login/routes/web.php
@@ -2,6 +2,14 @@
 
 use App\Http\Controllers\AuthController;
 
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('/dashboard', function () {
+    return "Login exitoso";
+});
+
 Route::get('/auth/discord', [AuthController::class, 'redirectDiscord']);
 Route::get('/auth/discord/callback', [AuthController::class, 'handleDiscordCallback']);
 
